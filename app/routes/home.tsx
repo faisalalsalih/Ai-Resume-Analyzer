dc0d9ad08fd8a69900bae6a47a442baf9a7bdeee
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-12-02 09:01:45 +0500

    ScoreCircle Added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -20,6 +22,17 @@ export default function Home() {
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
     </section>
+
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {
+          resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))
+        }
+      </div>
+    )}
 
   </main>;
 }
